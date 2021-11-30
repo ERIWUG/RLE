0f26f4a171ea49ee6c38d6cafda213638fab1243
ERIWUG <e.belyi2013@gmail.com>
2021-11-30 22:55:55 +0300

    Добавьте файлы проекта.

---
+++ RLE/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace RLE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 277);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "00009000009090009000900000900900090000000000909090000900090090009009009090090000";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Кодирование";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(233, 315);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(44, 21);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "int";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(233, 353);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(66, 21);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "String";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 344);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 30);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Декодирование";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(233, 392);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(57, 21);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Test";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(350, 13);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(865, 400);
+            this.textBox2.TabIndex = 8;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(13, 392);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1244, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ RLE/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+
+namespace RLE
+{
+    
+    public partial class Form1 : Form
+    {
+
+
+        public string k;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public int min(int a,int b)
+        {
+            return a < b ? a : b;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                string k = textBox1.Text;
+                string res = "";
+                string pr = k.Substring(0, 1);
+                int kol = 1;
+                int prev_kol = 0;
+                for (int i = 1; i < k.Length; i++)
+                {
+                    if (k.Substring(i, 1) == pr) { kol += 1; }
+                    else
+                    {
+                        if (prev_kol == 1) { res += pr; }
+                        else { res += Convert.ToString(kol) + pr; }
+                        pr = k.Substring(i, 1);
+                        prev_kol = kol;
+                        kol = 1;
+                    }
+                }
+                if (prev_kol == 1) { res += pr; }
+                else { res += Convert.ToString(kol) + pr; }
+                textBox2.Text = res;
+                
+            }
+            else if(radioButton1.Checked)
+            {
+                string k = textBox1.Text;
+                string s = "↑";
+                string res = "";
+                string pr = k.Substring(0, 1);
+                int kol = 1;
+                
+                for (int i = 1; i < k.Length; i++)
+                {
+                    textBox2.Text = textBox1.Text + "\r\n" + s+"\r\n"+res;
+                    
+                    s = "  " + s.Substring(0);
+                    if (k.Substring(i, 1) == pr) { kol += 1; }
+                    else
+                    {
+                        while (kol > 0)
+                        {
+                            res += Convert.ToString(min(kol, 9)) + pr;
+                            kol -= min(kol, 9);
+                        }
+
+                        pr = k.Substring(i, 1);
+                        
+                        kol = 1;
+                    }
+                    await Task.Delay(Convert.ToInt32(numericUpDown1.Value*100));
+                }
+
+                while (kol > 0)
+                {
+                    res += Convert.ToString(min(kol, 9)) + pr;
+                    kol -= min(kol, 9);
+                }
+                textBox2.Text = res;
+            }
+            else
+            {
+                string k = "↑";
+                
+                for(int i = 0; i <textBox1.Text.Length; i++)
+                {
+                    textBox2.Text = textBox1.Text + "\r\n" + k;
+                    k = "  " + k.Substring(0);
+                    await Task.Delay(300);
+                }
+             
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+    
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+
+            if (radioButton1.Checked)
+            {
+                string k = textBox1.Text;
+                string s = "↑";
+                string res="";
+                for (int i = 0; i < k.Length-1; i+=2)
+                {
+                    for(int j = Convert.ToInt32(k.Substring(i, 1)); j > 0; j--)
+                    {
+                        s= "  " + s.Substring(0);
+                        res += k.Substring(i + 1, 1);
+                    }
+                    textBox2.Text = k + "\r\n" + s + "\r\n" + res;
+                    await Task.Delay(Convert.ToInt32(numericUpDown1.Value * 100));
+                }
+                textBox2.Text = res;
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
